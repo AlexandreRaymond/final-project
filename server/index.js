dce018ca6bcb2245dd0ed26d5d88cc5884a0b918
“Alexandre <alexandre.raymond.103@gmail.com>
2023-03-30 20:48:02 -0400

    server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hi! banana");
+});
+
+app.listen(port, () => {
+  console.log(`example app listening on port ${port}`);
+});
